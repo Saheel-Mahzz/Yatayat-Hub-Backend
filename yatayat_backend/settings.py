2cb147ff76355784317c334b770ab45cf9f42cc0
saheel <programmingwithsahil@gmail.com>
2026-06-30 23:45:03 +0545

    fix: chanegs in the settings.py

--- yatayat_backend/settings.py
+++ yatayat_backend/settings.py
@@ -98,13 +98,19 @@ WSGI_APPLICATION = 'yatayat_backend.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600,
+#     )
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
