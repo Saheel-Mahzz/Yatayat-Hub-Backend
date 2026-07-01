4d538a7dc7e244244598dd6dd094427897a60259
saheel <programmingwithsahil@gmail.com>
2026-07-01 15:22:50 +0545

    fix: chanegs int eh settgin.py

--- yatayat_backend/settings.py
+++ yatayat_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&fcpr*#t0pjrv&4g+qawo^v!9p7ydywq5t^p1pmdqcn$p%#6i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://yatayat-hub-backend-2.onrender.com','https://yatayat-hub-backend-3.onrender.com','https://yatayat-hub-backend-4.onrender.com' 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['yatayat-hub-backend-2.onrender.com','yatayat-hub-backend-3.onrender.com','yatayat-hub-backend-4.onrender.com' 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -135,5 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOW_ALL_ORIGINS = True
